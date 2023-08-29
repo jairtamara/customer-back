0abb22bd8b89943b01b301b032876bf5787a2d1f
Jtamara <jairfabian93@gmail.com>
2023-08-29 11:25:11 -0500

    agregar comentarios

--- src/main/java/org/sotobotero/customer/CorsFilter.java
+++ src/main/java/org/sotobotero/customer/CorsFilter.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  *** Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.sotobotero.customer;
+//package org.sotobotero.customer;
 
-import jakarta.servlet.FilterChain;
+/*import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.filter.OncePerRequestFilter;***/
 
 /**
  *
